4815f90c55da85aa2289e586a946eccf6a33371d
Jason Rayles <jason.rayles@nbcuni.com>
2016-01-08 12:41:29 -0500

    Initial Commit

---
+++ Location Spoofer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location Spoofer
+//
+//  Created by Jason Rayles on 1/8/16.
+//  Copyright © 2016 Jason Rayles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
